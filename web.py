16ce4ebfef82932f91c3e4f9e190334d4166fc6f
Baze Petrushev <b.petrushev@gmail.com>
2012-01-14 19:12:01 +0100

    Added application factory; base front controller

--- web.py
+++ web.py
@@ -1,13 +1,20 @@
 
-from werkzeug.wrappers import Request, Response
-from werkzeug.utils import redirect as http_redirect
+from sys import modules
+
 from simplejson import dumps as json_dumps
 
+from werkzeug.wrappers import Request, Response
+from werkzeug.utils import redirect as http_redirect
+from werkzeug.exceptions import HTTPException
+
+
 def render(tpl_env, template, view):
+    """Renders a view into a template given a template environment `tpl_env """
     return tpl_env.get_template(template).render(**view)
 
 
 class BaseController(object):
+    """Extendable base controller"""
     
     def __init__(self, request, tpl_env, appspace, **kwargs):
         
@@ -46,13 +53,17 @@ class BaseController(object):
         return self._log
     
     def init(self):
+        """Executes before the action"""
         pass
     
     def exit(self):
+        """Executes after the action"""
         pass
 
     @property
     def response(self):
+        """Returns the response, if given a template,
+        renders a view and puts it as data of the response"""
         if self.template:
             self._response.data = render(self._tpl_env, self.template, self.view)
             self._response.content_length = len(self._response.data)
@@ -89,3 +100,55 @@ class BaseController(object):
         self._response.content_length = len(self._response.data)
         self._response.content_type="text/json"
 
+
+def get_application(FrontControllerClass, appspace):
+    """Retuns WSGI application given a front controller class 
+    and application namespace"""
+    
+    def application(environ, start_response):
+        request=Request(environ)
+        fc=FrontControllerClass(request, appspace)
+        return fc.response(environ, start_response)
+        
+    return application
+    
+
+class BaseFrontController(object):
+    """Basic front controller, `dispatch` should be extended"""
+
+    def __init__(self, request, appspace):
+        self._request=request
+        self.appspace=appspace
+        self._is_finished=False
+        
+    def dispatch(self):
+        raise NotImplementedError
+
+    def _default_dispatch(self, controllers_path):
+        """Returns controller class, action name and args given match on the rules"""
+        try:
+            endpoint, args = self.urls.match()
+        except HTTPException, exc:
+            endpoint, args = 'index|notfound', {}
+        
+        controller_name, action_name = endpoint.split('|')
+        
+        __import__(controllers_path+controller_name)
+        controller_module = modules[controllers_path+controller_name]
+        controller_class = controller_module.Controller
+
+        return controller_class, action_name, args
+
+        
+    @property
+    def response(self):
+        if not self._is_finished:
+            self.dispatch()
+            self._is_finished=True
+        return self._response
+    
+    @property
+    def request(self):
+        return self._request
+
+        
