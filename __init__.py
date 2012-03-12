1a7e18d76d0c325e7cf3c0e0e780f42200bc25a7
Baze Petrushev <b.petrushev@gmail.com>
2012-03-12 16:11:36 +0100

    Memoization routine

--- __init__.py
+++ __init__.py
@@ -8,3 +8,24 @@ def md5(text):
     m = hashlib_md5(text)
     return m.hexdigest()
 
+class Memo(object):
+    """Memoization singleton, holding cache for different function"""
+    _cache = {}
+
+    @classmethod
+    def memo(cls, fc):
+        """Memoization with cache by function"""
+        try:
+            fc_cache = cls._cache[id(fc)]
+        except KeyError:
+            fc_cache = cls._cache[id(fc)]={}
+        def memoized(*args):
+            try:
+                return fc_cache[tuple(args)]
+            except KeyError:
+                res = fc(*args)
+                fc_cache[tuple(args)]=res
+                return res
+        return memoized
+
+memoize = Memo.memo
